35d779bbcff193f639f214e561f5ab41cc6cdffa
FrancoFalk <francofalk@gmail.com>
2022-09-28 11:06:57 -0300

    Form Usuario-Empleado

--- AudioCorrientes/Login/CAdministrador/Empleados/AgregarEmpleados.Designer.cs
+++ AudioCorrientes/Login/CAdministrador/Empleados/AgregarEmpleados.Designer.cs
@@ -31,15 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.btnSalirMenuPrincipal = new System.Windows.Forms.Button();
             this.btnAgregarEmpleado = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.comboBoxRol = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtApellido = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtDni = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,57 +43,34 @@
             this.txtDireccion = new System.Windows.Forms.TextBox();
             this.txtTel = new System.Windows.Forms.TextBox();
             this.errorProviderAgregarEmpleado = new System.Windows.Forms.ErrorProvider(this.components);
-            this.btnVerPass = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.errorProviderAgregarEmpleado)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btnVerPass)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSalirMenuPrincipal
             // 
-            this.btnSalirMenuPrincipal.Location = new System.Drawing.Point(312, 460);
+            this.btnSalirMenuPrincipal.Location = new System.Drawing.Point(273, 319);
             this.btnSalirMenuPrincipal.Name = "btnSalirMenuPrincipal";
-            this.btnSalirMenuPrincipal.Size = new System.Drawing.Size(161, 44);
+            this.btnSalirMenuPrincipal.Size = new System.Drawing.Size(200, 44);
             this.btnSalirMenuPrincipal.TabIndex = 34;
             this.btnSalirMenuPrincipal.Text = "Salir";
             this.btnSalirMenuPrincipal.UseVisualStyleBackColor = true;
             // 
             // btnAgregarEmpleado
             // 
-            this.btnAgregarEmpleado.Location = new System.Drawing.Point(82, 460);
+            this.btnAgregarEmpleado.Location = new System.Drawing.Point(42, 319);
             this.btnAgregarEmpleado.Name = "btnAgregarEmpleado";
-            this.btnAgregarEmpleado.Size = new System.Drawing.Size(161, 44);
+            this.btnAgregarEmpleado.Size = new System.Drawing.Size(200, 44);
             this.btnAgregarEmpleado.TabIndex = 33;
             this.btnAgregarEmpleado.Text = "Agregar";
             this.btnAgregarEmpleado.UseVisualStyleBackColor = true;
             this.btnAgregarEmpleado.Click += new System.EventHandler(this.btnAgregarEmpleado_Click);
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(309, 152);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 30;
-            this.label7.Text = "Id Rol";
-            // 
-            // comboBoxRol
-            // 
-            this.comboBoxRol.FormattingEnabled = true;
-            this.comboBoxRol.Items.AddRange(new object[] {
-            "Guitarras Electricas",
-            "Guitarras Acusticas",
-            "Baterias",
-            "Bajos",
-            "Amplificadores"});
-            this.comboBoxRol.Location = new System.Drawing.Point(312, 168);
-            this.comboBoxRol.Name = "comboBoxRol";
-            this.comboBoxRol.Size = new System.Drawing.Size(161, 21);
-            this.comboBoxRol.TabIndex = 29;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(79, 329);
+            this.label6.Location = new System.Drawing.Point(270, 112);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(44, 13);
             this.label6.TabIndex = 28;
@@ -106,25 +79,16 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(79, 272);
+            this.label5.Location = new System.Drawing.Point(39, 231);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(44, 13);
             this.label5.TabIndex = 27;
             this.label5.Text = "Nombre";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(79, 211);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 13);
-            this.label4.TabIndex = 26;
-            this.label4.Text = "Contraseña";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(79, 152);
+            this.label2.Location = new System.Drawing.Point(39, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(26, 13);
             this.label2.TabIndex = 24;
@@ -132,33 +96,25 @@
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(82, 345);
+            this.txtApellido.Location = new System.Drawing.Point(273, 128);
             this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(161, 20);
+            this.txtApellido.Size = new System.Drawing.Size(200, 20);
             this.txtApellido.TabIndex = 22;
             this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(82, 288);
+            this.txtNombre.Location = new System.Drawing.Point(42, 247);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(161, 20);
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
             this.txtNombre.TabIndex = 21;
             this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
             // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(82, 227);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(161, 20);
-            this.txtPassword.TabIndex = 20;
-            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
-            // 
             // txtDni
             // 
-            this.txtDni.Location = new System.Drawing.Point(82, 168);
+            this.txtDni.Location = new System.Drawing.Point(42, 128);
             this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(161, 20);
+            this.txtDni.Size = new System.Drawing.Size(200, 20);
             this.txtDni.TabIndex = 19;
             this.txtDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDni_KeyPress);
             // 
@@ -166,7 +122,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(177, 36);
+            this.label1.Location = new System.Drawing.Point(156, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(207, 25);
             this.label1.TabIndex = 18;
@@ -175,7 +131,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(309, 272);
+            this.label3.Location = new System.Drawing.Point(270, 168);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 13);
             this.label3.TabIndex = 38;
@@ -184,7 +140,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(309, 211);
+            this.label9.Location = new System.Drawing.Point(39, 168);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(49, 13);
             this.label9.TabIndex = 37;
@@ -192,16 +148,16 @@
             // 
             // txtDireccion
             // 
-            this.txtDireccion.Location = new System.Drawing.Point(312, 288);
+            this.txtDireccion.Location = new System.Drawing.Point(273, 184);
             this.txtDireccion.Name = "txtDireccion";
-            this.txtDireccion.Size = new System.Drawing.Size(161, 20);
+            this.txtDireccion.Size = new System.Drawing.Size(200, 20);
             this.txtDireccion.TabIndex = 36;
             // 
             // txtTel
             // 
-            this.txtTel.Location = new System.Drawing.Point(312, 227);
+            this.txtTel.Location = new System.Drawing.Point(42, 184);
             this.txtTel.Name = "txtTel";
-            this.txtTel.Size = new System.Drawing.Size(161, 20);
+            this.txtTel.Size = new System.Drawing.Size(200, 20);
             this.txtTel.TabIndex = 35;
             this.txtTel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTel_KeyPress);
             // 
@@ -209,45 +165,45 @@
             // 
             this.errorProviderAgregarEmpleado.ContainerControl = this;
             // 
-            // btnVerPass
+            // label4
             // 
-            this.btnVerPass.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnVerPass.Image = global::Login.Properties.Resources.mostrar_contrasena;
-            this.btnVerPass.Location = new System.Drawing.Point(12, 211);
-            this.btnVerPass.Name = "btnVerPass";
-            this.btnVerPass.Size = new System.Drawing.Size(44, 36);
-            this.btnVerPass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.btnVerPass.TabIndex = 39;
-            this.btnVerPass.TabStop = false;
-            this.btnVerPass.Click += new System.EventHandler(this.btnVerPass_Click);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Fecha Nacimiento";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(273, 247);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 41;
             // 
             // AgregarEmpleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 540);
-            this.Controls.Add(this.btnVerPass);
+            this.ClientSize = new System.Drawing.Size(529, 385);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.txtDireccion);
             this.Controls.Add(this.txtTel);
             this.Controls.Add(this.btnSalirMenuPrincipal);
             this.Controls.Add(this.btnAgregarEmpleado);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.comboBoxRol);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtApellido);
             this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtDni);
             this.Controls.Add(this.label1);
             this.Name = "AgregarEmpleados";
             this.Text = "AgregarEmpleados";
             ((System.ComponentModel.ISupportInitialize)(this.errorProviderAgregarEmpleado)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btnVerPass)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,15 +213,11 @@
 
         private System.Windows.Forms.Button btnSalirMenuPrincipal;
         private System.Windows.Forms.Button btnAgregarEmpleado;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBoxRol;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtApellido;
         private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.TextBox txtDni;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
@@ -273,6 +225,7 @@
         private System.Windows.Forms.TextBox txtDireccion;
         private System.Windows.Forms.TextBox txtTel;
         private System.Windows.Forms.ErrorProvider errorProviderAgregarEmpleado;
-        private System.Windows.Forms.PictureBox btnVerPass;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
     }
 }
---
+++ AudioCorrientes/Login/CSuAdministrador/Usuarios/AgregarRol.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Login.CSuAdministrador.Usuarios
+{
+    partial class AgregarRol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.btnSalirMenuPrincipal = new System.Windows.Forms.Button();
+            this.btnAgregarEmpleado = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxRol = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnVerPass = new System.Windows.Forms.PictureBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            ((System.ComponentModel.ISupportInitialize)(this.btnVerPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Direccion";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(317, 193);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 55;
+            this.label9.Text = "Telefono";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(320, 270);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.ReadOnly = true;
+            this.txtDireccion.Size = new System.Drawing.Size(161, 20);
+            this.txtDireccion.TabIndex = 54;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(320, 209);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.ReadOnly = true;
+            this.txtTel.Size = new System.Drawing.Size(161, 20);
+            this.txtTel.TabIndex = 53;
+            // 
+            // btnSalirMenuPrincipal
+            // 
+            this.btnSalirMenuPrincipal.Location = new System.Drawing.Point(320, 442);
+            this.btnSalirMenuPrincipal.Name = "btnSalirMenuPrincipal";
+            this.btnSalirMenuPrincipal.Size = new System.Drawing.Size(161, 44);
+            this.btnSalirMenuPrincipal.TabIndex = 52;
+            this.btnSalirMenuPrincipal.Text = "Salir";
+            this.btnSalirMenuPrincipal.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarEmpleado
+            // 
+            this.btnAgregarEmpleado.Location = new System.Drawing.Point(90, 442);
+            this.btnAgregarEmpleado.Name = "btnAgregarEmpleado";
+            this.btnAgregarEmpleado.Size = new System.Drawing.Size(161, 44);
+            this.btnAgregarEmpleado.TabIndex = 51;
+            this.btnAgregarEmpleado.Text = "Agregar";
+            this.btnAgregarEmpleado.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(317, 134);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Id Rol";
+            // 
+            // comboBoxRol
+            // 
+            this.comboBoxRol.FormattingEnabled = true;
+            this.comboBoxRol.Items.AddRange(new object[] {
+            "Guitarras Electricas",
+            "Guitarras Acusticas",
+            "Baterias",
+            "Bajos",
+            "Amplificadores"});
+            this.comboBoxRol.Location = new System.Drawing.Point(320, 150);
+            this.comboBoxRol.Name = "comboBoxRol";
+            this.comboBoxRol.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxRol.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 311);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(87, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "DNI";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(90, 327);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.ReadOnly = true;
+            this.txtApellido.Size = new System.Drawing.Size(161, 20);
+            this.txtApellido.TabIndex = 44;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(90, 270);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ReadOnly = true;
+            this.txtNombre.Size = new System.Drawing.Size(161, 20);
+            this.txtNombre.TabIndex = 43;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(90, 209);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtPassword.TabIndex = 42;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(90, 150);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(161, 20);
+            this.txtDni.TabIndex = 41;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 25);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Agregar Usuario";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 44);
+            this.button1.TabIndex = 58;
+            this.button1.Text = "Buscar Empleado";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnVerPass
+            // 
+            this.btnVerPass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVerPass.Image = global::Login.Properties.Resources.mostrar_contrasena;
+            this.btnVerPass.Location = new System.Drawing.Point(20, 193);
+            this.btnVerPass.Name = "btnVerPass";
+            this.btnVerPass.Size = new System.Drawing.Size(44, 36);
+            this.btnVerPass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnVerPass.TabIndex = 57;
+            this.btnVerPass.TabStop = false;
+            // 
+            // AgregarRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 496);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnVerPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.btnSalirMenuPrincipal);
+            this.Controls.Add(this.btnAgregarEmpleado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBoxRol);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.label1);
+            this.Name = "AgregarRol";
+            this.Text = "AgregarRol";
+            ((System.ComponentModel.ISupportInitialize)(this.btnVerPass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox btnVerPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Button btnSalirMenuPrincipal;
+        private System.Windows.Forms.Button btnAgregarEmpleado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxRol;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
